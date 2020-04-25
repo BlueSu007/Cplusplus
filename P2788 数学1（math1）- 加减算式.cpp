65189099febe206e00b56dad4bbdb0d8040080d5
BlueSu007 <62138430+BlueSu007@users.noreply.github.com>
2020-04-25 19:46:08 +0800

    Create P2788 数学1（math1）- 加减算式.cpp

---
+++ P2788 数学1（math1）- 加减算式.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cstdio>
+#include <cmath>
+#include <algorithm>
+using namespace std;
+
+int main(){
+	
+	int cnt = 0, tmp;
+	while(cin >> tmp)
+		cnt += tmp;
+		
+	cout << cnt << endl;
+	
+	return 0;
+}
