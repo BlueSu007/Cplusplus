7881a3d132b99638f2328173a0e8f034a009003d
BlueSu007 <62138430+BlueSu007@users.noreply.github.com>
2020-04-30 21:30:29 +0800

    Create P1048 采药.cpp

---
+++ P1048 采药.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <cstdio>
+#include <cmath>
+#include <algorithm> 
+using namespace std;
+
+int m, n;
+int v[30000 + 5], w[30000 + 5];
+int f[30000 + 5];
+
+int main(){
+	
+	cin >> m >> n;
+	
+	for(int i = 1; i <= n; i++){
+		cin >> v[i] >> w[i];
+	}
+	
+	for(int i = 1; i <= n; i++){
+		for(int j = m; j >= v[i]; j--){
+			if(j >= v[i])
+				f[j] = max(f[j], f[j - v[i]] + w[i]);
+		}
+	}
+	
+	cout << f[m] << endl;
+	
+	return 0;
+}
