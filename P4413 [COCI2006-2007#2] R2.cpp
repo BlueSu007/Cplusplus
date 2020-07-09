7b08a7ac620b3fdf117772945b93ef6d312ee2bd
BlueSu007 <62138430+BlueSu007@users.noreply.github.com>
2020-07-09 21:58:19 +0800

    Create P4413 [COCI2006-2007#2] R2.cpp

---
+++ P4413 [COCI2006-2007#2] R2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int leftn , s;
+
+int main() {
+	
+	scanf ( "%d %d" , &leftn , &s );
+	printf ( "%d" ,  ( s * 2 ) - leftn );
+	
+	return 0;
+}
