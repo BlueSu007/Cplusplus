02fdcfb33a2fa50385e36abffed08fc5a2321533
BlueSu007 <62138430+BlueSu007@users.noreply.github.com>
2020-08-09 17:05:34 +0800

    Create P2089 烤鸡.cpp

---
+++ P2089 烤鸡.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <cstdio>
+#include <cmath>
+#include <algorithm>
+using namespace std;
+
+int I_do_not_know_how_to_name_this_variable ;
+int x ;
+
+int main () {
+
+    scanf ( "%d" , &I_do_not_know_how_to_name_this_variable ) ;
+
+    for ( int a = 1 ; a <= 3 ; a++ )
+    for ( int b = 1 ; b <= 3 ; b++ )
+    for ( int c = 1 ; c <= 3 ; c++ )
+    for ( int d = 1 ; d <= 3 ; d++ )
+    for ( int e = 1 ; e <= 3 ; e++ )
+    for ( int f = 1 ; f <= 3 ; f++ )
+    for ( int g = 1 ; g <= 3 ; g++ )
+    for ( int h = 1 ; h <= 3 ; h++ )
+    for ( int i = 1 ; i <= 3 ; i++ )
+    for ( int j = 1 ; j <= 3 ; j++ )
+    if ( a + b + c + d + e + f + g + h + i + j == I_do_not_know_how_to_name_this_variable )
+    x++ ;
+
+    printf ( "%d\n" , x ) ;
+
+    for ( int a = 1 ; a <= 3 ; a++ )
+    for ( int b = 1 ; b <= 3 ; b++ )
+    for ( int c = 1 ; c <= 3 ; c++ )
+    for ( int d = 1 ; d <= 3 ; d++ )
+    for ( int e = 1 ; e <= 3 ; e++ )
+    for ( int f = 1 ; f <= 3 ; f++ )
+    for ( int g = 1 ; g <= 3 ; g++ )
+    for ( int h = 1 ; h <= 3 ; h++ )
+    for ( int i = 1 ; i <= 3 ; i++ )
+    for ( int j = 1 ; j <= 3 ; j++ )
+    if ( a + b + c + d + e + f + g + h + i + j == I_do_not_know_how_to_name_this_variable )
+    {
+        printf ( "%d %d %d %d %d %d %d %d %d %d\n" , a , b , c , d , e , f , g , h , i , j ) ;
+    }
+
+    return 0 ;
+}
