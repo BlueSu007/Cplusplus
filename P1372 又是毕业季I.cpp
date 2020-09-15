dd300a2a90045fa5586cc71ac7fe5ad53f937977
BlueSu007 <62138430+BlueSu007@users.noreply.github.com>
2020-09-15 15:25:06 +0800

    Create P1372 又是毕业季I.cpp

---
+++ P1372 又是毕业季I.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cstdio>
+#include <cmath>
+#include <algorithm>
+using namespace std;
+
+int n , m ;
+
+int main () {
+
+    cin >> n >> m ;
+    cout << n / m << endl ;
+
+    return 0 ;
+}
