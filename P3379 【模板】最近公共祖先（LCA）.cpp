da7065a1dce172ca3600ebe1b34f72b3c6dde64f
BlueSu007 <62138430+BlueSu007@users.noreply.github.com>
2020-12-04 21:48:49 +0800

    Update P3379 【模板】最近公共祖先（LCA）.cpp

--- P3379 【模板】最近公共祖先（LCA）.cpp
+++ P3379 【模板】最近公共祖先（LCA）.cpp
@@ -5,43 +5,46 @@
 #include <climits>
 using namespace std;
 
-#define LEN 500005
-int n , q , root , u , v , head[LEN] , nextn[LEN * 2] , endn[LEN * 2] , cnt ;
-int depth[LEN] , father[LEN] ;
+#define LEN 500100
+int n , q , u , v , root , depth[LEN] , f[LEN][20] ;
+int head[LEN] , nextn[LEN * 2] , endn[LEN * 2] , cnt ;
 inline void addEdge ( int u , int v ) ;
-inline void dfs ( int x ) ;
+inline void dfs ( int cur ) ;
 inline int LCA ( int u , int v ) ;
 
 int main () {
 
     scanf ( "%d %d %d" , &n , &q , &root ) ;
-    for ( int i = 1 ; i < n ; i++ ) {
+    for ( int i = 1 ; i < n ; ++ i ) {
         scanf ( "%d %d" , &u , &v ) ;
         addEdge ( u , v ) , addEdge ( v , u ) ;
     }
 
     dfs ( root ) ;
 
-    for ( int i = 1 ; i <= q ; i++ ) {
+    for ( int i = 1 ; i <= q ; ++ i ) {
         scanf ( "%d %d" , &u , &v ) ;
-        int tmp = LCA ( u , v ) ;
-        printf ( "%d\n" , tmp ) ;
+        printf ( "%d\n" , LCA ( u , v ) ) ;
     }
 
     return 0 ;
 }
 
 inline void addEdge ( int u , int v ) {
-    cnt++ , nextn[cnt] = head[u] ;
+    ++ cnt , nextn[cnt] = head[u] ;
     head[u] = cnt , endn[cnt] = v ;
 }
 
-inline void dfs ( int x ) {
-    depth[x] = depth[father[x]] + 1 ;
-    for ( int i = head[x] ; i ; i = nextn[i] ) {
-        if ( endn[i] != father[x] ) {
-            father[endn[i]] = x ;
-            dfs ( endn[i] ) ;
+inline void dfs ( int cur ) {
+    depth[cur] = depth[f[cur][0]] + 1 ;
+    for ( int j = 1 ; j <= 19 ; ++ j ) {
+        f[cur][j] = f[f[cur][j - 1]][j - 1] ;
+    }
+
+    for ( int j = head[cur] ; j > 0 ; j = nextn[j] ) {
+        if ( endn[j] != f[cur][0] ) {
+            f[endn[j]][0] = cur ;
+            dfs ( endn[j] ) ;
         }
     }
 }
@@ -52,15 +55,23 @@ inline int LCA ( int u , int v ) {
             swap ( u , v ) ;
         }
 
-        while ( depth[u] > depth[v] ) {
-            u = father[u] ;
+        for ( int j = 19 ; j >= 0 ; -- j ) {
+            if ( depth[f[u][j]] >= depth[v] ) {
+                u = f[u][j] ;
+            }
         }
     }
 
-    while ( u != v ) {
-        u = father[u] ;
-        v = father[v] ;
+    if ( u == v ) {
+        return u ;
     }
 
-    return u ;
+    for ( int j = 19 ; j >= 0 ; -- j ) {
+        if ( f[u][j] != f[v][j] ) {
+            u = f[u][j] ;
+            v = f[v][j] ;
+        }
+    }
+
+    return f[u][0] ;
 }
