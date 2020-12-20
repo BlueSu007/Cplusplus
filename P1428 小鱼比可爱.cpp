08a65d8d0f0f99f6ff9e5ae2b0472f207d31468f
BlueSu007 <62138430+BlueSu007@users.noreply.github.com>
2020-12-20 16:15:28 +0800

    Create P1428 小鱼比可爱.cpp

---
+++ P1428 小鱼比可爱.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+	int n;
+	cin>>n;
+
+	int a[n],b[n]={0};
+
+	for(int i=0;i<n;i++){
+		cin>>a[i];
+	}
+
+	for(int i=0;i<n;i++){
+		for(int j=i;j>=0;j--){
+			if(a[i]>a[j]){
+				b[i]++;
+			}
+		}
+	}
+
+	for(int i=0;i<n;i++){
+		cout<<b[i]<<" ";
+	}
+
+	return 0;
+}
