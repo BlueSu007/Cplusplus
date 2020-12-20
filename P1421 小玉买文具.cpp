ce5b70d463df9d52978a0c6604b17dfb892dbfe6
BlueSu007 <62138430+BlueSu007@users.noreply.github.com>
2020-12-20 16:08:22 +0800

    Create P1421 小玉买文具.cpp

---
+++ P1421 小玉买文具.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main(){
+    
+    int n,m;
+    double a;
+    
+    cin>>n>>m;
+    a=n+m/10.0;
+    
+    cout<<floor(a/1.9)<<endl;
+    
+    return 0;
+    
+}
