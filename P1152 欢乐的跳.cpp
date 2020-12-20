431828def8b681d1a5da1e75b801f6660213fea3
BlueSu007 <62138430+BlueSu007@users.noreply.github.com>
2020-12-20 15:49:35 +0800

    Create P1152 欢乐的跳.cpp

---
+++ P1152 欢乐的跳.cpp
@@ -0,0 +1,33 @@
+#include<cstdio>
+#include<algorithm>
+#include<cmath>
+using namespace std;
+
+int a[1005],c[1005];
+
+int main(){
+    int n;
+    scanf("%d",&n);
+
+    for(int i=1;i<=n;i++){
+        scanf("%d",&a[i]);
+    }
+
+    for(int i=1;i<n;i++){
+        c[i]=abs(a[i]-a[i+1]);//处理差
+    }
+
+    sort(c+1,c+n);
+
+    for(int i=1;i<n;i++){
+        if(c[i]!=i){
+			printf("Not jolly\n");
+			return 0;
+		}//判断输出
+    }
+
+    printf("Jolly\n");//如果未能输出，则为正确答案
+
+    return 0;
+
+}
