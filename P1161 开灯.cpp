7be6b8d459a2fe2694f343416d647907855a924d
BlueSu007 <62138430+BlueSu007@users.noreply.github.com>
2020-12-20 15:50:10 +0800

    Create P1161 开灯.cpp

---
+++ P1161 开灯.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int a[20200330],n;
+double x,y;
+
+int main(){
+	
+    cin>>n;
+    
+    for(int i=1;i<=n;i++){
+        cin>>x>>y;
+        
+        for(double j=1;j<=y;++j){
+            if(a[int(j*x)]==0) a[int(j*x)]=1;
+            else a[int(j*x)]=0;
+        }
+    }
+    
+    for(int i=1;;i++){
+        if(a[i]==1){
+            cout<<i<<endl;
+            break;
+        }
+    }
+    
+    return 0;
+} 
