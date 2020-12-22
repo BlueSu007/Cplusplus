c84a8b23a70eadd5e36e461d1be3ad88a3bb4062
BlueSu007 <62138430+BlueSu007@users.noreply.github.com>
2020-12-22 21:02:30 +0800

    Create CF0664A Complicated GCD.cpp

---
+++ CF0664A Complicated GCD.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cstdio>
+#include <cmath>
+#include <algorithm> 
+#include <climits>
+#include <cstring>
+#include <string>
+using namespace std;
+
+string a , b ;
+
+int main () {
+
+    ios :: sync_with_stdio ( false ) ;
+    cin >> a >> b ;
+    if ( a == b ) {
+        cout << a << endl ;
+    }
+    else {
+        cout << 1 << endl ;
+    }
+
+    return 0 ;
+}
